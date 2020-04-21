0343f6a5c827ce0886d97bc75087ab295be8468e
BelyashMatvey <belyakov06mat@mail.ru>
2020-04-21 19:04:38 +0300

    blog

---
+++ blog/landing/forms.py
@@ -0,0 +1,15 @@
+from django import forms  
+from .models import Comment
+from django.contrib.postgres.search import SearchVector 
+from .models import Post 
+class CommentForm(forms.ModelForm):  
+    class Meta:  
+        model = Comment  
+        fields = ('name', 'email', 'body')
+class EmailPostForm(forms.Form):  
+    name = forms.CharField(max_length=25)  
+    email = forms.EmailField()  
+    to = forms.EmailField()  
+    comments = forms.CharField(required=False,  widget=forms.Textarea)
+class SearchForm(forms.Form): 
+    query = forms.CharField()
